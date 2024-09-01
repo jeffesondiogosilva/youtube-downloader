c59d1ff03f25155abc5f56af5f188dfb51700456
jeff <diogoworkss@gmail.com>
2024-08-31 21:59:11 -0300

    fix: tentativa de refatorar para remoto - 1

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template, request, send_file
 import yt_dlp
 import os
 import tempfile
+import logging
+
+# Configuração básica do logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
 
-# Diretório para salvar os vídeos (será criado no diretório temporário)
+# Diretório para salvar os vídeos
 DOWNLOAD_DIR = tempfile.mkdtemp()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,12 +21,14 @@ def index():
     if request.method == 'POST':
         video_url = request.form['url']
         try:
-            # Obter informações do vídeo usando yt-dlp
+            logging.debug(f"Received video URL: {video_url}")
             with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
                 info_dict = ydl.extract_info(video_url, download=False)
                 formats = info_dict.get('formats', [])
+                logging.debug(f"Available formats: {formats}")
         except Exception as e:
-            return f"Error: {e}"
+            logging.error(f"Error occurred while fetching video formats: {e}")
+            return render_template('index.html', video_url=video_url, formats=formats, error_message=f"Error: {e}")
 
     return render_template('index.html', video_url=video_url, formats=formats)
 
@@ -30,22 +37,24 @@ def download_video():
     video_url = request.form['url']
     format_id = request.form['format_id']
     try:
-        # Opções de download
+        logging.debug(f"Downloading video URL: {video_url} with format ID: {format_id}")
         ydl_opts = {
             'format': format_id,
-            'outtmpl': os.path.join(DOWNLOAD_DIR, '%(title)s.%(ext)s'),  # Salvar apenas no diretório temporário
+            'outtmpl': os.path.join(DOWNLOAD_DIR, '%(title)s.%(ext)s'),
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(video_url, download=True)
             filename = ydl.prepare_filename(info_dict)
 
-        # Verifica se o arquivo existe no diretório de downloads e envia o arquivo
         filepath = os.path.join(DOWNLOAD_DIR, os.path.basename(filename))
         if os.path.exists(filepath):
+            logging.info(f"File successfully downloaded: {filepath}")
             return send_file(filepath, as_attachment=True)
         else:
+            logging.error("File not found after download")
             return "Error: File not found", 404
     except Exception as e:
+        logging.error(f"Error occurred during download: {e}")
         return f"Error: {e}"
 
 if __name__ == '__main__':
